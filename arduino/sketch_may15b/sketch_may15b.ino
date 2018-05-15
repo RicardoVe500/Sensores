b61f38519afeeda89e336e941bc57a533937c6e7
RicardoVe500 <ricardillo500@gmail.com>
2018-05-15 15:16:38 -0600

    imprimiendo numeros en arduino

--- arduino/sketch_may15b/sketch_may15b.ino
+++ arduino/sketch_may15b/sketch_may15b.ino
@@ -7,8 +7,20 @@ void setup() {
   Serial.begin(9600);
 }
 
+int Residuo [4]= {0,0,0,0};
 void loop() {
-  int Valor1 = analogRead(Sensor1);
-  Serial.println(Valor1);
+ int Valor1 =1234; //analogRead(Sensor1);
+  //int Residuo = Valor1%10;
+  for(int i = 0; i<4; i++ ){
+    Residuo[i]= Valor1%10;
+    Valor1 = Valor1/10;
+     
+    }
+    for(int i = 3; i>=0; i--)
+    
+    Serial.print(Residuo[i]);
+    //Serial.println(Residuo[i]);
+    Serial.println("*");
+  //Serial.write(Valor1);
   delay(1000);
 }
